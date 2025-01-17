0394cd79b0bd8d9fbe1ceaaed4eacb816784430a
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-17 10:00:39 -0500

    e

--- Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
+++ Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
@@ -1,7 +1,6 @@
 term.clear()
 term.setCursorPos(1,1)
 
-local file = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/Starlight_OS_Adv/Main/1.0.0/Installer/FS.json")
-local fh = fs.open("/temp/FS.json", "w")
-fh.write(file.readAll())
-fh.close()
+local FS = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/Starlight_OS_Adv/Main/1.0.0/Installer/FS.json")
+
+
