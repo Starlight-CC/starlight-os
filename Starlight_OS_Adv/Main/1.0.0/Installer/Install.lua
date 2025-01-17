77d2e055cbacaacc65b9079b44c36500a4ef7513
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-17 10:37:14 -0500

    e

--- Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
+++ Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
@@ -6,6 +6,10 @@ term.write("Starlight OS")
 term.setTextColor(colors.white)
 term.write("!")
 term.setCursorPos(1,2)
+sleep(1.5)
+print("formating FS")
+sleep(.5)
+print("SFS Formated")
 
 local FS = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/Starlight_OS_Adv/Main/1.0.0/Installer/FS.json")
 local FS = FS.readAll()
@@ -15,5 +19,10 @@ l1 = FS[2]
 
 for i,v in ipairs(l1) do
     FS.makeDir(v)
-    print()
+    term.write("[ ")
+    term.setTextColor(colors.green)
+    term.write("OK")
+    term.setTextColor(colors.white)
+    term.write(" ] "..v)
+    term.setCursorPos(1,2)
 end
