393c71ca859098f736d25d74e5e5ced68587ae66
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-17 10:54:26 -0500

    e

--- Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
+++ Starlight_OS_Adv/Main/1.0.0/Installer/Install.lua
@@ -10,19 +10,50 @@ sleep(1.5)
 print("formating FS")
 sleep(.5)
 print("SFS Formated")
+print("making Directorys")
 
-local FS = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/Starlight_OS_Adv/Main/1.0.0/Installer/FS.json")
-local FS = FS.readAll()
+--local FS = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/Starlight_OS_Adv/Main/1.0.0/Installer/FS.json")
+local FS = {
+    "",
+    {
+        "/",
+        "/bin/",
+        "/lib/",
+        "/boot/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/root/",
+        "/sys/"
+    }
+}
 
 root = FS[1]
 l1 = FS[2]
 
 for i,v in ipairs(l1) do
-    FS.makeDir(v)
+    local x,y = term.getCursorPos()
+    fs.makeDir(v)
     term.write("[ ")
     term.setTextColor(colors.green)
     term.write("OK")
     term.setTextColor(colors.white)
-    term.write(" ] "..v)
-    term.setCursorPos(1,2)
+    term.write(" ] mkDir "..v)
+    if y == 19 then
+        term.scroll()
+        term.setCursorPos(1,19)
+    else
+        term.setCursorPos(1,y+1)
+    end
+    sleep(0)
 end
+
+files = FS[2]
+
