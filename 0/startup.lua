ae7ce7891b1ab40c69242602875b0d6769768f85
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-23 09:04:44 -0500

    e

--- 0/startup.lua
+++ 0/startup.lua
@@ -1,15 +1,21 @@
-local root = "sys/shell/rom/programs/"
+local roota = "/sys/shell/rom/programs/"
+local rootc = "sys/shell/rom/programs/"
 local completion = require("cc.shell.completion")
 
 shell.clearAlias("ls")
-shell.setAlias("ls",root.."list.lua")
-shell.setAlias("list",root.."list.lua")
-shell.setCompletionFunction(root.."list.lua", completion.build(completion.dir))
+shell.setAlias("ls",roota.."list.lua")
+shell.setAlias("list",roota.."list.lua")
+shell.setCompletionFunction(rootc.."list.lua", completion.build(completion.dir))
 
-shell.setAlias("cd",root.."cd.lua")
-shell.setCompletionFunction("rom/programs/cd.lua", completion.build(completion.dir))
+shell.clearAlias("sh")
+shell.setAlias("sh",roota.."shell.lua")
+shell.setAlias("shell",roota.."shell.lua")
 
-shell.setAlias("lua",root.."lua.lua")
+shell.setAlias("cd",roota.."cd.lua")
+shell.setCompletionFunction(rootc.."cd.lua", completion.build(completion.dir))
+
+shell.setAlias("lua",roota.."lua.lua")
+shell.setAlias("pwd",roota.."pwd.lua")
 
 term.clear()
 term.setCursorPos(1,1)
