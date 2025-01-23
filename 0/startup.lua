17356f2e34648581c96a60d6206a436af5ae275a
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-01-23 06:06:49 -0500

    Add files via upload

---
+++ 0/startup.lua
@@ -0,0 +1,12 @@
+local root = "sys/shell/rom/programs/"
+local completion = require("cc.shell.completion")
+
+shell.clearAlias("ls")
+shell.setAlias("ls",root.."list.lua")
+shell.setCompletionFunction(root.."list.lua", completion.build(completion.dir))
+
+shell.setAlias("lua",root.."lua.lua")
+
+term.clear()
+term.setCursorPos(1,1)
+shell.run("/sys/shell/shell.lua")
