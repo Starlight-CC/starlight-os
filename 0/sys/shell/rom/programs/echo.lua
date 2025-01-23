c16176359078b650803e1eeb6ee61185f686cb7c
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-23 14:12:22 -0500

    e

--- 0/sys/shell/rom/programs/echo.lua
+++ 0/sys/shell/rom/programs/echo.lua
@@ -1,6 +1,7 @@
 local tArgs = { ... }
 if #tArgs < 1 then
     local programName = arg[0] or fs.getName(shell.getRunningProgram())
+    print("Usage: [arg] <string>")
     print("Usage: <string>")
     return
 end
@@ -18,37 +19,59 @@ local function move(ix,iy)
 end
 
 local function go()
-    for i,v in ipairs(tArgs) do
-        if v == "/n" then
-            local x,y = term.getCursorPos()
-            term.setCursorPos(1,y)
-            move(0,1)
-        elseif v == "/c" then
-            return "c"
-        elseif v == "/b" then
-            move(-1,0)
-        elseif v == "/t" then
-            move(4,0)
-        elseif v == "/v" then
-            vc = vc+1
-            local x,y = term.getCursorPos()
-            term.setCursorPos(1,y)
-            move(vc*4,1)
-        else
+    if tArgs[1] == "-e" then
+        for i,v in ipairs(tArgs) do
+
+            if v == "/n" then
+                local x,y = term.getCursorPos()
+                term.setCursorPos(1,y)
+                move(0,1)
+            elseif v == "/c" then
+                return "c"
+            elseif v == "/b" then
+                move(-1,0)
+            elseif v == "/t" then
+                move(4,0)
+            elseif v == "/v" then
+                vc = vc+1
+                local x,y = term.getCursorPos()
+                term.setCursorPos(1,y)
+                move(vc*4,1)
+            elseif i == 1 then
+                --remove arg
+            else
+                term.write(v.." ")
+            end
+        end
+    elseif tArgs[1] == "-n" then
+        for i,v in ipairs(tArgs) do
+            if i == 1 then
+                --remove arg
+            else
+                term.write(v.." ")
+            end
+        end
+        return "c"
+    elseif tArgs[1] == "*" then
+        for i,v in ipairs(tArgs) do
+            if i == 1 then
+                --remove arg
+            else
+                textutils.pagedTabulate(fs.list(shell.dir()))
+            end
+        end
+    else
+        for i,v in ipairs(tArgs) do
             term.write(v.." ")
         end
     end
 end
 
 local x,y = term.getCursorPos()
-local ret = go()
-if not ret == "c" then
+if go() == nil then
     local x,y = term.getCursorPos()
-    if y == 19 then
-        term.scroll(1)
-    else
-        term.setCursorPos(1,y+1)
-    end
+    term.setCursorPos(1,y)
+    move(0,1)
 end
 
 
