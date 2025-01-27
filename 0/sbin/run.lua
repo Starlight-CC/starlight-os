2226c2ef2ad50373b9859523ad248da8d084164a
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-27 09:52:18 -0500

    e

--- 0/sbin/run.lua
+++ 0/sbin/run.lua
@@ -6,13 +6,17 @@ if #tArgs < 1 then
     return
 end
 
-local file = fs.open(tArgs[1],"r")
-local text = ""
-while true do
-  text = file.readLine(true)
-  if text == "end" then
-    break
-  else
-    shell.run(text)
+if string.sub(tArgs[1],#tArgs[1]-2) == ".sh" then
+  local file = fs.open(tArgs[1],"r")
+  local text = ""
+  while true do
+    text = file.readLine(true)
+    if text == "end" then
+      break
+    else
+      shell.run(text)
+    end
   end
+else
+  shell.run(tArgs[1])
 end
