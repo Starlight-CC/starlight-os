e34e5f8aebb4e2aae39db16a9df4054f3d9f9ca3
Astronand <hunter.vinson@fwcsstudents.org>
2025-01-28 08:42:42 -0500

    e

--- 0/bin/flappybird/main.lua
+++ 0/bin/flappybird/main.lua
@@ -6,32 +6,45 @@ term.clear()
 local bird = {}
 local FBL = require("/bin/flappybird/FBlib")
 local pipe = FBL.pipe
-while true do
-  term.setBackgroundColor(colors.lightBlue)
-  term.clear()
-  bird = {
-    4,2,
-    {
-      "3","4","f","3",
-      "4","4","4","1",
-    },
-    {
-      "4","4","0","1",
-      "3","4","4","3",
-    },
-    {
-      7,5,4,11,
-      11,9,10,4,
+local function tick()
+  while true do
+    term.setBackgroundColor(colors.lightBlue)
+    term.clear()
+    bird = {
+      4,2,
+      {
+        "3","4","f","3",
+        "4","4","4","1",
+      },
+      {
+        "4","4","0","1",
+        "3","4","4","3",
+      },
+      {
+        7,5,4,11,
+        11,9,10,4,
+      }
     }
-  }
-  glib.drawSprite(pipe,x,6)
-  glib.drawSprite(pipe,x+5,6)
-  glib.drawSprite(pipe,x+10,-10)
-  glib.drawSprite(pipe,x+15,6)
-  glib.drawSprite(pipe,x+20,6)
-  glib.drawSprite(pipe,x+25,6)
-  glib.drawSprite(bird,5,y)
-  sleep(1)
-  y = y + 1
-  x = x - 1
+    glib.drawSprite(pipe,x,6)
+    glib.drawSprite(pipe,x+5,6)
+    glib.drawSprite(pipe,x+10,-10)
+    glib.drawSprite(pipe,x+15,6)
+    glib.drawSprite(pipe,x+20,6)
+    glib.drawSprite(pipe,x+25,6)
+    glib.drawSprite(bird,5,y)
+    glib.printScreenBuffer()
+    sleep(1)
+    y = y + 1
+    x = x - 1
+  end
 end
+local function input()
+  while true do
+    local i1,i2 = os.pullEvent()
+    if i1 == "key" and i2 == keys.space then
+      y = y - 2
+    end
+    sleep(0)
+  end
+end
+parallel.waitForAny(tick, input)
--- 0/lib/glib.lua
+++ 0/lib/glib.lua
@@ -1,25 +1,49 @@
 local lib = {}
-function lib.drawSprite(t,ox,oy)
-  local tx = t[1]
-  local ty = t[2]
-  local x = 1
-  local y = 1
-  local idx = 1
-  while y < ty + 1 do
-    while x < tx + 1 do
-      term.setCursorPos(ox+x,oy+y)
-      term.setTextColor(colors.fromBlit(t[3][idx]))
-      term.setBackgroundColor(colors.fromBlit(t[4][idx]))
-      term.write(string.char(128 + t[5][idx]))
-      x = x + 1
-      idx = idx + 1
+lib.sprite = {}
+lib.buffer = {}
+function lib.sprite.draw(t,ox,oy,b)
+  if b == nil then
+    local tx = t[1]
+    local ty = t[2]
+    local x = 1
+    local y = 1
+    local idx = 1
+    while y < ty + 1 do
+      while x < tx + 1 do
+        term.setCursorPos(ox+x,oy+y)
+        term.setTextColor(colors.fromBlit(t[3][idx]))
+        term.setBackgroundColor(colors.fromBlit(t[4][idx]))
+        term.write(string.char(128 + t[5][idx]))
+        x = x + 1
+        idx = idx + 1
+      end
+      y = y + 1
+      x = 1
     end
-    y = y + 1
-    x = 1
+  else
+    local a = b
+    local tx = t[1]
+    local ty = t[2]
+    local x = 1
+    local y = 1
+    local idx = 1
+    while y < ty + 1 do
+      while x < tx + 1 do
+        local tbx,tby = ox+x,oy+y
+        a[tostring(tbx)..","..tostring(tby).."t"] = colors.fromBlit(t[3][idx])
+        a[tostring(tbx)..","..tostring(tby).."b"] = colors.fromBlit(t[4][idx])
+        a[tostring(tbx)..","..tostring(tby).."c"] = string.char(128 + t[5][idx])
+        x = x + 1
+        idx = idx + 1
+      end
+      y = y + 1
+      x = 1
+    end
+    return a
   end
 end
 
-lib.exampleSprite = {
+lib.sprite.example = {
   4,4,
   {
     "f","f","f","f",
@@ -40,5 +64,28 @@ lib.exampleSprite = {
     12,13,14,15
   }
 }
+
+function lib.buffer.print(b)
+
+end
+
+function lib.buffer.create()
+  local a = {}
+  local x = 1
+  local y = 1
+  local tx,ty = term.getSize()
+  while y < ty + 1 do
+    while x < tx + 1 do
+      local tbx,tby = x,y
+      a[tostring(tbx)..","..tostring(tby).."t"] = "f"
+      a[tostring(tbx)..","..tostring(tby).."b"] = "f"
+      a[tostring(tbx)..","..tostring(tby).."c"] = 0
+      x = x + 1
+    end
+    y = y + 1
+    x = 1
+  end
+  return a
+end
 
 return lib
