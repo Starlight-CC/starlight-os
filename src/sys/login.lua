177897550b7cfd75a736d3ce1500db52147639bf
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-01-31 18:36:14 -0500

    e

--- src/sys/login.lua
+++ src/sys/login.lua
@@ -3,27 +3,27 @@
 -- edit os API
 
 function os.username(e)
-    if user == nil then
-        user = e
-        return true
-    else
-        return user
-    end
+  if user == nil then
+    user = e
+    return true
+  else
+    return user
+  end
 end
 
 function os.hostname(e)
-    if host == nil then
-        host = e
-        return true
-    else
-        return host
-    end
+  if host == nil then
+    host = e
+    return true
+  else
+    return host
+  end
 end
 
 function os.getUsers()
-    file = fs.open(".users","r")
-    local e = file.readAll()
-    file.close()
-    return e 
+  file = fs.open(".users","r")
+  local e = file.readAll()
+  file.close()
+  return e 
 end
 
