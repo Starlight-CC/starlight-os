07f617fed34f9f429b0c44da9100b875e0ac976f
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-11 10:54:51 -0500

    e

--- src/installer/fs.lua
+++ src/installer/fs.lua
@@ -1,25 +1,18 @@
 {
-  --base
   {
-    "/",
-    "/bin/",
-    "/home/",
-    "/lib/",
-    "/sbin/",
-    "/sys/",
-    "/usr/",
-    "/var/"
-  },
-  --sys bin
-  {
-    "startup.lua"
-  },
-  --shell
-  {
-    
-  },
-  --usr
-  {
-
+    "startup.lua",
+    "sys/login.lua",
+    "sys/startup.lua",
+    "usr/sbin/shell/shell.lua",
+    "usr/sbin/shell/about.lua",
+    "usr/sbin/shell/alias.lua",
+    "usr/sbin/shell/apis.lua",
+    "usr/sbin/shell/cd.lua",
+    "usr/sbin/shell/clear.lua",
+    "usr/sbin/shell/copy.lua",
+    "usr/sbin/shell/delete.lua",
+    "usr/sbin/shell/drive.lua",
+    "usr/sbin/shell/echo.lua",
+    "usr/sbin/shell/edit.lua"
   }
 }
---
+++ src/installer/install.lua
@@ -0,0 +1,10 @@
+local repoPath = "https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/root/"
+
+local iso = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/installer/fs.lua")
+
+for i,v in ipairs(iso) do
+    local file = http.get(repoPath..v)
+    local fh = fs.open(v, "w")
+    fh.write(file.readAll())
+    fh.close()
+end
