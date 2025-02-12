d8afb91e9928fc8e5e67a9ffc23ddab63dca1018
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-12 10:27:08 -0500

    w

---
+++ Versions/1.0.0/root/sys/login.lua
@@ -0,0 +1,30 @@
+--os.pullEvent = os.pullEventRaw
+
+-- edit os API
+
+function os.username(e)
+  if user == nil then
+    user = e
+    return true
+  else
+    return user
+  end
+end
+
+function os.hostname(e)
+  if host == nil then
+    host = e
+    return true
+  else
+    return host
+  end
+end
+
+function os.getUsers()
+  file = fs.open(".users","r")
+  local e = file.readAll()
+  file.close()
+  return e 
+end
+
+if f
