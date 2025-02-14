5886f8d5826a86c49bdbc35102a08aac7cbe918b
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-14 10:54:38 -0500

    e

---
+++ src/root/usr/sbin/shell/shell copy.lua
@@ -0,0 +1,602 @@
+
+term.clear()
+term.setCursorPos(1,1)
+
+local make_package = dofile("rom/modules/main/cc/require.lua").make
+
+local multishell = multishell
+local parentShell = shell
+local parentTerm = term.current()
+
+if multishell then
+    multishell.setTitle(multishell.getCurrent(), "shell")
+end
+
+local bExit = false
+local sDir = parentShell and parentShell.dir() or ""
+local sPath = parentShell and parentShell.path() or ".:/rom/programs"
+local tAliases = parentShell and parentShell.aliases() or {}
+local tCompletionInfo = parentShell and parentShell.getCompletionInfo() or {}
+local tProgramStack = {}
+
+local shell = {} --- @export
+local function createShellEnv(dir)
+    local env = { shell = shell, multishell = multishell }
+    env.require, env.package = make_package(env, dir)
+    return env
+end
+
+-- Set up a dummy require
+local require
+do
+    local env = setmetatable(createShellEnv("/rom/programs"), { __index = _ENV })
+    require = env.require
+end
+local expect = require("cc.expect").expect
+local exception = require "cc.internal.exception"
+
+-- Colors
+local promptColour, textColour, bgColour
+if term.isColour() then
+    promptColour = colours.cyan
+    textColour = colours.white
+    bgColour = colours.black
+else
+    promptColour = colours.white
+    textColour = colours.white
+    bgColour = colours.black
+end
+
+local function tokenise(...)
+    local sLine = table.concat({ ... }, " ")
+    local tWords = {}
+    local bQuoted = false
+    for match in string.gmatch(sLine .. "\"", "(.-)\"") do
+        if bQuoted then
+            table.insert(tWords, match)
+        else
+            for m in string.gmatch(match, "[^ \t]+") do
+                table.insert(tWords, m)
+            end
+        end
+        bQuoted = not bQuoted
+    end
+    return tWords
+end
+
+-- Execute a program
+local function executeProgram(remainingRecursion, path, args)
+    local file, err = fs.open(path, "r")
+    if not file then
+        printError(err)
+        return false
+    end
+
+    -- First check if the file begins with a #!
+    local contents = file.readLine() or ""
+
+    if contents:sub(1, 2) == "#!" then
+        file.close()
+
+        remainingRecursion = remainingRecursion - 1
+        if remainingRecursion == 0 then
+            printError("Hashbang recursion depth limit reached when loading file: " .. path)
+            return false
+        end
+
+        -- Load the specified hashbang program instead
+        local hashbangArgs = tokenise(contents:sub(3))
+        local originalHashbangPath = table.remove(hashbangArgs, 1)
+        local resolvedHashbangProgram = shell.resolveProgram(originalHashbangPath)
+        if not resolvedHashbangProgram then
+            printError("Hashbang program not found: " .. originalHashbangPath)
+            return false
+        elseif resolvedHashbangProgram == "rom/programs/shell.lua" and #hashbangArgs == 0 then
+            -- If we try to launch the shell then our shebang expands to "shell <program>", which just does a
+            -- shell.run("<program>") again, resulting in an infinite loop. This may still happen (if the user
+            -- has a custom shell), but this reduces the risk.
+            -- It's a little ugly special-casing this, but it's probably worth warning about.
+            printError("Cannot use the shell as a hashbang program")
+            return false
+        end
+
+        -- Add the path and any arguments to the interpreter's arguments
+        table.insert(hashbangArgs, path)
+        for _, v in ipairs(args) do
+            table.insert(hashbangArgs, v)
+        end
+
+        hashbangArgs[0] = originalHashbangPath
+        return executeProgram(remainingRecursion, resolvedHashbangProgram, hashbangArgs)
+    end
+
+    contents = contents .. "\n" .. (file.readAll() or "")
+    file.close()
+
+    local dir = fs.getDir(path)
+    local env = setmetatable(createShellEnv(dir), { __index = _G })
+    env.arg = args
+
+    local func, err = load(contents, "@/" .. path, nil, env)
+    if not func then
+        -- We had a syntax error. Attempt to run it through our own parser if
+        -- the file is "small enough", otherwise report the original error.
+        if #contents < 1024 * 128 then
+            local parser = require "cc.internal.syntax"
+            if parser.parse_program(contents) then printError(err) end
+        else
+            printError(err)
+        end
+
+        return false
+    end
+
+    if settings.get("bios.strict_globals", false) then
+        getmetatable(env).__newindex = function(_, name)
+            error("Attempt to create global " .. tostring(name), 2)
+        end
+    end
+
+    local ok, err, co = exception.try(func, table.unpack(args, 1, args.n))
+
+    if ok then return true end
+
+    if err and err ~= "" then
+        printError(err)
+        exception.report(err, co)
+    end
+
+    return false
+end
+
+--- Run a program with the supplied arguments.
+function shell.execute(command, ...)
+    expect(1, command, "string")
+    for i = 1, select('#', ...) do
+        expect(i + 1, select(i, ...), "string")
+    end
+
+    local sPath = shell.resolveProgram(command)
+    if sPath ~= nil then
+        tProgramStack[#tProgramStack + 1] = sPath
+        if multishell then
+            local sTitle = fs.getName(sPath)
+            if sTitle:sub(-4) == ".lua" then
+                sTitle = sTitle:sub(1, -5)
+            end
+            multishell.setTitle(multishell.getCurrent(), sTitle)
+        end
+
+        local result = executeProgram(100, sPath, { [0] = command, ... })
+
+        tProgramStack[#tProgramStack] = nil
+        if multishell then
+            if #tProgramStack > 0 then
+                local sTitle = fs.getName(tProgramStack[#tProgramStack])
+                if sTitle:sub(-4) == ".lua" then
+                    sTitle = sTitle:sub(1, -5)
+                end
+                multishell.setTitle(multishell.getCurrent(), sTitle)
+            else
+                multishell.setTitle(multishell.getCurrent(), "shell")
+            end
+        end
+        return result
+       else
+        printError("No such program")
+        return false
+    end
+end
+
+function shell.run(...)
+    local tWords = tokenise(...)
+    local sCommand = tWords[1]
+    if sCommand then
+        return shell.execute(sCommand, table.unpack(tWords, 2))
+    end
+    return false
+end
+
+--- Exit the current shell.
+function shell.exit()
+    bExit = true
+end
+
+--- Return the current working directory.
+function shell.dir()
+    return sDir
+end
+
+--- Set the current working directory.
+function shell.setDir(dir)
+    expect(1, dir, "string")
+    if not fs.isDir(dir) then
+        error("Not a directory", 2)
+    end
+    sDir = fs.combine(dir, "")
+end
+
+--- Set the path where programs are located.
+function shell.path()
+    return sPath
+end
+
+--- Set the [current program path][`path`].
+function shell.setPath(path)
+    expect(1, path, "string")
+    sPath = path
+end
+
+--- Resolve a relative path to an absolute path.
+function shell.resolve(path)
+    expect(1, path, "string")
+    local sStartChar = string.sub(path, 1, 1)
+    if sStartChar == "/" or sStartChar == "\\" then
+        return fs.combine("", path)
+    else
+        return fs.combine(sDir, path)
+    end
+end
+
+local function pathWithExtension(_sPath, _sExt)
+    local nLen = #sPath
+    local sEndChar = string.sub(_sPath, nLen, nLen)
+    -- Remove any trailing slashes so we can add an extension to the path safely
+    if sEndChar == "/" or sEndChar == "\\" then
+        _sPath = string.sub(_sPath, 1, nLen - 1)
+    end
+    return _sPath .. "." .. _sExt
+end
+
+--- Resolve a program, using the [program path][`path`] and list of [aliases][`aliases`].
+function shell.resolveProgram(command)
+    expect(1, command, "string")
+    -- Substitute aliases firsts
+    if tAliases[command] ~= nil then
+        command = tAliases[command]
+    end
+
+    -- If the path is a global path, use it directly
+    if command:find("/") or command:find("\\") then
+        local sPath = shell.resolve(command)
+        if fs.exists(sPath) and not fs.isDir(sPath) then
+            return sPath
+        else
+            local sPathLua = pathWithExtension(sPath, "lua")
+            if fs.exists(sPathLua) and not fs.isDir(sPathLua) then
+                return sPathLua
+            end
+        end
+        return nil
+    end
+
+     -- Otherwise, look on the path variable
+    for sPath in string.gmatch(sPath, "[^:]+") do
+        sPath = fs.combine(shell.resolve(sPath), command)
+        if fs.exists(sPath) and not fs.isDir(sPath) then
+            return sPath
+        else
+            local sPathLua = pathWithExtension(sPath, "lua")
+            if fs.exists(sPathLua) and not fs.isDir(sPathLua) then
+                return sPathLua
+            end
+        end
+    end
+
+    -- Not found
+    return nil
+end
+
+--- Return a list of all programs on the [path][`shell.path`].
+function shell.programs(include_hidden)
+    expect(1, include_hidden, "boolean", "nil")
+
+    local tItems = {}
+
+    -- Add programs from the path
+    for sPath in string.gmatch(sPath, "[^:]+") do
+        sPath = shell.resolve(sPath)
+        if fs.isDir(sPath) then
+            local tList = fs.list(sPath)
+            for n = 1, #tList do
+                local sFile = tList[n]
+                if not fs.isDir(fs.combine(sPath, sFile)) and
+                   (include_hidden or string.sub(sFile, 1, 1) ~= ".") then
+                    if #sFile > 4 and sFile:sub(-4) == ".lua" then
+                        sFile = sFile:sub(1, -5)
+                    end
+                    tItems[sFile] = true
+                end
+            end
+        end
+    end
+
+    -- Sort and return
+    local tItemList = {}
+    for sItem in pairs(tItems) do
+        table.insert(tItemList, sItem)
+    end
+    table.sort(tItemList)
+    return tItemList
+end
+
+local function completeProgram(sLine)
+    local bIncludeHidden = settings.get("shell.autocomplete_hidden")
+    if #sLine > 0 and (sLine:find("/") or sLine:find("\\")) then
+        -- Add programs from the root
+        return fs.complete(sLine, sDir, {
+            include_files = true,
+            include_dirs = false,
+            include_hidden = bIncludeHidden,
+        })
+
+    else
+        local tResults = {}
+        local tSeen = {}
+
+        -- Add aliases
+        for sAlias in pairs(tAliases) do
+            if #sAlias > #sLine and string.sub(sAlias, 1, #sLine) == sLine then
+                local sResult = string.sub(sAlias, #sLine + 1)
+                if not tSeen[sResult] then
+                    table.insert(tResults, sResult)
+                    tSeen[sResult] = true
+                end
+            end
+        end
+
+        -- Add all subdirectories. We don't include files as they will be added in the block below
+        local tDirs = fs.complete(sLine, sDir, {
+            include_files = false,
+            include_dirs = false,
+            include_hidden = bIncludeHidden,
+        })
+        for i = 1, #tDirs do
+            local sResult = tDirs[i]
+            if not tSeen[sResult] then
+                table.insert (tResults, sResult)
+                tSeen [sResult] = true
+            end
+        end
+
+        -- Add programs from the path
+        local tPrograms = shell.programs()
+        for n = 1, #tPrograms do
+            local sProgram = tPrograms[n]
+            if #sProgram > #sLine and string.sub(sProgram, 1, #sLine) == sLine then
+                local sResult = string.sub(sProgram, #sLine + 1)
+                if not tSeen[sResult] then
+                    table.insert(tResults, sResult)
+                    tSeen[sResult] = true
+                end
+            end
+        end
+
+        -- Sort and return
+        table.sort(tResults)
+        return tResults
+    end
+end
+
+local function completeProgramArgument(sProgram, nArgument, sPart, tPreviousParts)
+    local tInfo = tCompletionInfo[sProgram]
+    if tInfo then
+        return tInfo.fnComplete(shell, nArgument, sPart, tPreviousParts)
+    end
+    return nil
+end
+
+--- Complete a shell command line.
+function shell.complete(sLine)
+    expect(1, sLine, "string")
+    if #sLine > 0 then
+        local tWords = tokenise(sLine)
+        local nIndex = #tWords
+        if string.sub(sLine, #sLine, #sLine) == " " then
+            nIndex = nIndex + 1
+        end
+        if nIndex == 1 then
+            local sBit = tWords[1] or ""
+            local sPath = shell.resolveProgram(sBit)
+            if tCompletionInfo[sPath] then
+                return { " " }
+            else
+                local tResults = completeProgram(sBit)
+                for n = 1, #tResults do
+                    local sResult = tResults[n]
+                    local sPath = shell.resolveProgram(sBit .. sResult)
+                    if tCompletionInfo[sPath] then
+                        tResults[n] = sResult .. " "
+                    end
+                end
+                return tResults
+            end
+
+        elseif nIndex > 1 then
+            local sPath = shell.resolveProgram(tWords[1])
+            local sPart = tWords[nIndex] or ""
+            local tPreviousParts = tWords
+            tPreviousParts[nIndex] = nil
+            return completeProgramArgument(sPath , nIndex - 1, sPart, tPreviousParts)
+
+        end
+    end
+    return nil
+end
+
+--- Complete the name of a program.
+function shell.completeProgram(program)
+    expect(1, program, "string")
+    return completeProgram(program)
+end
+
+--- Set the completion function for a program.
+function shell.setCompletionFunction(program, complete)
+    expect(1, program, "string")
+    expect(2, complete, "function")
+    tCompletionInfo[program] = {
+        fnComplete = complete,
+    }
+end
+
+--- Get a table containing all completion functions.
+function shell.getCompletionInfo()
+    return tCompletionInfo
+end
+
+--- Returns the path to the currently running program.
+function shell.getRunningProgram()
+    if #tProgramStack > 0 then
+        return tProgramStack[#tProgramStack]
+    end
+    return nil
+end
+
+--- Add an alias for a program.
+function shell.setAlias(command, program)
+    expect(1, command, "string")
+    expect(2, program, "string")
+    tAliases[command] = program
+end
+
+--- Remove an alias.
+function shell.clearAlias(command)
+    expect(1, command, "string")
+    tAliases[command] = nil
+end
+
+--- Get the current aliases for this shell.
+function shell.aliases()
+    -- Copy aliases
+    local tCopy = {}
+    for sAlias, sCommand in pairs(tAliases) do
+        tCopy[sAlias] = sCommand
+    end
+    return tCopy
+end
+
+if multishell then
+    --- Open a new [`multishell`] tab running a command.
+    --
+    -- This behaves similarly to [`shell.run`], but instead returns the process
+    -- index.
+    --
+    -- This function is only available if the [`multishell`] API is.
+    --
+    -- @tparam string ... The command line to run.
+    -- @see shell.run
+    -- @see multishell.launch
+    -- @since 1.6
+    -- @usage Launch the Lua interpreter and switch to it.
+    --
+    --     local id = shell.openTab("lua")
+    --     shell.switchTab(id)
+    function shell.openTab(...)
+        local tWords = tokenise(...)
+        local sCommand = tWords[1]
+        if sCommand then
+            local sPath = shell.resolveProgram(sCommand)
+            if sPath == "rom/programs/shell.lua" then
+                return multishell.launch(createShellEnv("rom/programs"), sPath, table.unpack(tWords, 2))
+            elseif sPath ~= nil then
+                return multishell.launch(createShellEnv("rom/programs"), "rom/programs/shell.lua", sCommand, table.unpack(tWords, 2))
+            else
+                printError("No such program")
+            end
+        end
+    end
+
+    --- Switch to the [`multishell`] tab with the given index.
+    --
+    -- @tparam number id The tab to switch to.
+    -- @see multishell.setFocus
+    -- @since 1.6
+    function shell.switchTab(id)
+        expect(1, id, "number")
+        multishell.setFocus(id)
+    end
+end
+
+local tArgs = { ... }
+if #tArgs > 0 then
+    -- "shell x y z"
+    -- Run the program specified on the commandline
+    shell.run(...)
+
+else
+    local function show_prompt()
+        term.setBackgroundColor(bgColour)
+        term.setTextColour(promptColour)
+        local pdir = ""
+        local idk = string.sub(shell.dir(),1,5+#os.username())
+        if idk == "home/"..os.username() then
+            local so = string.sub(shell.dir(),5+#os.username()+1)
+            pdir = "~"..so
+        else
+            pdir = "/"..shell.dir()
+        end
+        write(os.username().."@"..os.hostname()..":"..pdir.."$")
+        term.setTextColour(textColour)
+    end
+
+    -- "shell"
+    -- Print the header
+    term.setBackgroundColor(bgColour)
+    term.setTextColour(promptColour)
+    term.setTextColour(textColour)
+
+    -- Run the startup program
+    if parentShell == nil then
+        shell.run("/rom/startup.lua")
+    end
+
+    -- Read commands and execute them
+    local tCommandHistory = {}
+    while not bExit do
+        term.redirect(parentTerm)
+        show_prompt()
+
+
+        local complete
+        if settings.get("shell.autocomplete") then complete = shell.complete end
+
+        local ok, result
+        local co = coroutine.create(read)
+        assert(coroutine.resume(co, nil, tCommandHistory, complete))
+
+        while coroutine.status(co) ~= "dead" do
+            local event = table.pack(os.pullEvent())
+            if event[1] == "file_transfer" then
+                -- Abandon the current prompt
+                local _, h = term.getSize()
+                local _, y = term.getCursorPos()
+                if y == h then
+                    term.scroll(1)
+                    term.setCursorPos(1, y)
+                else
+                    term.setCursorPos(1, y + 1)
+                end
+                term.setCursorBlink(false)
+
+                -- Run the import script with the provided files
+                local ok, err = require("cc.internal.import")(event[2].getFiles())
+                if not ok and err then printError(err) end
+
+                -- And attempt to restore the prompt.
+                show_prompt()
+                term.setCursorBlink(true)
+                event = { "term_resize", n = 1 } -- Nasty hack to force read() to redraw.
+            end
+
+            if result == nil or event[1] == result or event[1] == "terminate" then
+                ok, result = coroutine.resume(co, table.unpack(event, 1, event.n))
+                if not ok then error(result, 0) end
+            end
+        end
+
+        if result:match("%S") and tCommandHistory[#tCommandHistory] ~= result then
+            table.insert(tCommandHistory, result)
+        end
+        shell.run(result)
+    end
+end
