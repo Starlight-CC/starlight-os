2bd99f1bf0bdb606aeb2889edb78cbe03784e9fe
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-17 10:26:44 -0500

    e

--- src/root/sys/serv/reboot.lua
+++
@@ -1,8 +0,0 @@
-if term.isColour() then
-    term.setTextColour(colours.blue)
-end
-print("rebooting")
-term.setTextColour(colours.white)
-
-sleep(1)
-os.reboot()
--- src/root/sys/serv/shutdown.lua
+++
@@ -1,8 +0,0 @@
-if term.isColour() then
-    term.setTextColour(colours.blue)
-end
-print("goodbye")
-term.setTextColour(colours.white)
-
-sleep(1)
-os.shutdown()
