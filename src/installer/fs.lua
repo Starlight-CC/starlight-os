5c323f2392b151ac01527eca9fda5de8a6d35f30
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-17 08:10:38 -0500

    e

--- src/installer/fs.lua
+++ src/installer/fs.lua
@@ -1,21 +1,7 @@
 local ret = {
   {
-    "startup.lua",
-    "sys/login.lua",
-    "sys/startup.lua",
-    "usr/sbin/shell/shell.lua",
-    "usr/sbin/shell/about.lua",
-    "usr/sbin/shell/alias.lua",
-    "usr/sbin/shell/apis.lua",
-    "usr/sbin/shell/cd.lua",
-    "usr/sbin/shell/clear.lua",
-    "usr/sbin/shell/copy.lua",
-    "usr/sbin/shell/delete.lua",
-    "usr/sbin/shell/drive.lua",
-    "usr/sbin/shell/echo.lua",
-    "usr/sbin/shell/edit.lua",
     "sys/serv/reboot.lua",
-    "test.lua"
+    "sys/serv/shutdown.lua"
   }
 }
 
