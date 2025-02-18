30c12b3ae9351e24b29d6b3aba33adbd8ae70f9f
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-18 07:57:47 -0500

    e

---
+++ src/install/data/fs1.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "boot/bios.lua",
+        "sbin/shell/shell.lua",
+        "sys/modules/internal/exception.lua",
+        "sys/modules/expect.lua",
+        "sys/modules/require.lua"
+    }
+}
