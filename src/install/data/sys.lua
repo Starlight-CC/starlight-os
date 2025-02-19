976606bd332c6384b98331069f32623402f9c4bc
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-19 14:12:29 -0500

    e

--- src/install/data/sys.lua
+++ src/install/data/sys.lua
@@ -19,5 +19,6 @@ return {
         "sys/modules/require.la",
         "sys/serv/reboot.lua",
         "sys/serv/shutdown.lua"
-    }
+    },
+    "https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/install/data/fs.lua"
 }
--- src/install/install.lua
+++ src/install/install.lua
@@ -2,14 +2,14 @@ local repoPath = "https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/
 local start = false
 if fs.exists("tmp/installerData.lua") then
     local list = loadfile("tmp/installerData.lua")
-    local iso = list()
+    iso = list()
 else
     local file = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/install/data/sys.lua")
     local fh = fs.open("tmp/installerData.lua", "w")
     fh.write(file.readAll())
     fh.close()
     local list = loadfile("tmp/installerData.lua")
-    local iso = list()
+    iso = list()
     start = true
 end
 if iso[2] == "end" then
