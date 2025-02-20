69b862b10e77f1a99ad7d23d4a0437d3ab3610c4
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-20 10:05:12 -0500

    e

--- src/install/install.lua
+++ src/install/install.lua
@@ -1,89 +1,321 @@
-local repoPath = "https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/root/"
-local start = false
-if fs.exists("tmp/installerData.lua") then
-    local list = loadfile("tmp/installerData.lua")
-    iso = list()
-else
-    local file = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/install/data/sys.lua")
-    local fh = fs.open("tmp/installerData.lua", "w")
-    fh.write(file.readAll())
-    fh.close()
-    local list = loadfile("tmp/installerData.lua")
-    iso = list()
-    start = true
+os.util = {}
+os._auth = {}
+local expect = dofile("sys/modules/expect.la")
+local expect, field = expect.expect, expect.field
+function os.version()
+    return "SLK 1.0.0"
 end
-term.setPaletteColor(colors.red,0xff0000)
-term.setPaletteColor(colors.green,0x00ff00)
-term.setPaletteColor(colors.blue,0x0000ff)
-term.clear()
-term.setCursorPos(1,1)
-if start then
-    term.write("Welcome to")
-    term.setTextColor(colors.blue)
-    term.write(" Starlight OS")
-    print("")
-    sleep(1)
+
+function os.pullEventRaw( sFilter )
+    return coroutine.yield( sFilter )
 end
-for i,v in ipairs(iso[1]) do
-    local file = http.get(repoPath..v)
-    if file == nil then 
-        term.setTextColor(colors.red) 
-        print(v..", failed to reach web address")
-        fs.delete(v)
-    else 
-        term.setTextColor(colors.green)
-        print(v) 
-        local fh = fs.open(v, "w")
-        fh.write(file.readAll())
-        fh.close()
+
+function os.pullEvent( sFilter )
+    local eventData = table.pack( os.pullEventRaw( sFilter ) )
+    if eventData[1] == "terminate" then
+        error( "Terminated", 0 )
+    end
+    return table.unpack( eventData, 1, eventData.n )
+end
+
+loadfile = function( _sFile, _tEnv )
+    if type( _sFile ) ~= "string" then
+        error( "bad argument #1 (expected string, got " .. type( _sFile ) .. ")", 2 ) 
+    end
+    if _tEnv ~= nil and type( _tEnv ) ~= "table" then
+        error( "bad argument #2 (expected table, got " .. type( _tEnv ) .. ")", 2 ) 
+    end
+    local file = fs.open( _sFile, "r" )
+    if file then
+        local func, err = load( file.readAll(), fs.getName( _sFile ), "t", _tEnv )
+        file.close()
+        return func, err
+    end
+    return nil, "File not found"
+end
+
+dofile = function( _sFile )
+    if type( _sFile ) ~= "string" then
+        error( "bad argument #1 (expected string, got " .. type( _sFile ) .. ")", 2 ) 
+    end
+    local fnFile, e = loadfile( _sFile, _G )
+    if fnFile then
+        return fnFile()
+    else
+        error( e, 2 )
     end
 end
-term.setTextColor(colors.green)
-for i,v in ipairs(iso[3]) do
-    print(v)
-    shell.run(v)
+
+local nativeShutdown = os.shutdown
+function os.shutdown()
+    nativeShutdown()
+    while true do
+        coroutine.yield()
+    end
 end
-fs.delete("tmp/installerData.lua")
-if iso[2] == "end" then
-    term.setPaletteColor(colors.red,0xff0000)
-    term.setPaletteColor(colors.green,0x00ff00)
-    term.setPaletteColor(colors.blue,0x0000ff)
-    term.clear()
-    term.setCursorPos(1,1)
-    term.write("Welcome to")
-    term.setTextColor(colors.blue)
-    term.write(" Starlight OS")
-    print("")
-    term.setTextColor(colors.green)
-    sleep(1)
-    fs.move("startup.lua","sbin/SLInstall.lua")
-    fs.move("sys/startup.lua","startup.lua")
-    fs.delete("tmp/installerData.lua")
-    print("Install complete rebooting...")
-    sleep(1)
-    print("SL.reboot service started")
-    shell.run("/sys/serv/reboot.lua")
+
+local nativeReboot = os.reboot
+function os.reboot()
+    nativeReboot()
+    while true do
+        coroutine.yield()
+    end
 end
-local file = http.get(iso[2])
-local fh = fs.open("tmp/installerData.lua", "w")
-fh.write(file.readAll())
-fh.close()
-if start then
-    if fs.exists("startup.lua") then
-        if fs.exists("startup.lua.old") then
-            fs.delete("startup.lua.old")
+
+function os.username(e)
+  if user == nil then
+    user = e
+    return true
+  else
+    return user
+  end
+end
+os.username("root")
+
+function os.hostname(e)
+  if host == nil then
+    host = e
+    return true
+  else
+    return host
+  end
+end
+os.hostname("CC")
+
+function os.getUsers()
+  file = fs.open("/.users","r")
+  local e = file.readAll()
+  file.close()
+  return e 
+end
+
+function os.home(e)
+    if os.username() == nil then
+        return "root/"
+    end
+    if e == nil then
+        if os.username() == "root" then
+            return "root/"
+        else
+            return "home/"..os.username()
         end
-        fs.move("startup.lua","startup.lua.old")
+    else
+        if e == "root" then
+            return "root/"
+        else
+            return "home/"..e
+        end
     end
-    fs.move(shell.getRunningProgram(),"startup.lua")
-    term.setTextColor(colors.white)
-    print("system will restart in 3")
-    sleep(1)
-    print("2")
-    sleep(1)
-    print("1")
-    sleep(1)
 end
-term.setTextColor(colors.green)
-print("SL.reboot service started")
-shell.run("/sys/serv/reboot.lua")
+
+function os.util.subHome(e)
+    local ret = ""
+    local idk = string.sub(e,1,#os.home())
+    if idk == os.home() then
+        local so = string.sub(e,#os.home()+1)
+        ret = "~"..so
+    else
+        ret = "/"..e
+    end
+    return ret
+end
+
+function os._auth.verify(e)
+end
+
+function os._auth.set(e)
+end
+
+function os._auth.checkFilePerms(e)
+end
+
+function os.help(e)
+    if os.username() == nil then
+        return "root/"
+    end
+    if e == nil then
+        print("The OS API is how programs")
+        print("interface with the kernel.")
+        print("anything with a '_' at the start")
+        print("of the function is only meant to")
+        print("be used by the os. if a program")
+        print("uses them it will be terminated.")
+    else
+        if fs.exists("/sys/help/"..e..".txt") then
+            local File = fs.open("/sys/help/"..e..".txt", "rb")
+            print(File.readAll())
+            File.close()
+        else
+            term.setTextColor(colors.red)
+            print("No help file found")
+        end
+    end
+end
+
+function fs.complete(sPath, sLocation, bIncludeFiles, bIncludeDirs)
+    expect(1, sPath, "string")
+    expect(2, sLocation, "string")
+    local bIncludeHidden = nil
+    if type(bIncludeFiles) == "table" then
+        bIncludeDirs = field(bIncludeFiles, "include_dirs", "boolean", "nil")
+        bIncludeHidden = field(bIncludeFiles, "include_hidden", "boolean", "nil")
+        bIncludeFiles = field(bIncludeFiles, "include_files", "boolean", "nil")
+    else
+        expect(3, bIncludeFiles, "boolean", "nil")
+        expect(4, bIncludeDirs, "boolean", "nil")
+    end
+
+    bIncludeHidden = bIncludeHidden ~= false
+    bIncludeFiles = bIncludeFiles ~= false
+    bIncludeDirs = bIncludeDirs ~= false
+    local sDir = sLocation
+    local nStart = 1
+    local nSlash = string.find(sPath, "[/\\]", nStart)
+    if nSlash == 1 then
+        sDir = ""
+        nStart = 2
+    end
+    local sName
+    while not sName do
+        local nSlash = string.find(sPath, "[/\\]", nStart)
+        if nSlash then
+            local sPart = string.sub(sPath, nStart, nSlash - 1)
+            sDir = fs.combine(sDir, sPart)
+            nStart = nSlash + 1
+        else
+            sName = string.sub(sPath, nStart)
+        end
+    end
+
+    if fs.isDir(sDir) then
+        local tResults = {}
+        if bIncludeDirs and sPath == "" then
+            table.insert(tResults, ".")
+        end
+        if sDir ~= "" then
+            if sPath == "" then
+                table.insert(tResults, bIncludeDirs and ".." or "../")
+            elseif sPath == "." then
+                table.insert(tResults, bIncludeDirs and "." or "./")
+            end
+        end
+        local tFiles = fs.list(sDir)
+        for n = 1, #tFiles do
+            local sFile = tFiles[n]
+            if #sFile >= #sName and string.sub(sFile, 1, #sName) == sName and (
+                bIncludeHidden or sFile:sub(1, 1) ~= "." or sName:sub(1, 1) == "."
+            ) then
+                local bIsDir = fs.isDir(fs.combine(sDir, sFile))
+                local sResult = string.sub(sFile, #sName + 1)
+                if bIsDir then
+                    table.insert(tResults, sResult .. "/")
+                    if bIncludeDirs and #sResult > 0 then
+                        table.insert(tResults, sResult)
+                    end
+                else
+                    if bIncludeFiles and #sResult > 0 then
+                        table.insert(tResults, sResult)
+                    end
+                end
+            end
+        end
+        return tResults
+    end
+
+    return {}
+end
+
+function os.about()
+    if fs.exists("/sys/about.txt") then
+        file = fs.open("/sys/about.txt","r")
+        e = file.readAll
+        file.close()
+        return e
+    else
+        return "file not found"
+    end
+end
+
+-- Load APIs
+local bAPIError = false
+local tApis = fs.list("/lib/apis")
+for n,sFile in ipairs( tApis ) do
+    if string.sub( sFile, 1, 1 ) ~= "." then
+        local sPath = fs.combine( "/lib/apis", sFile )
+        if not fs.isDir( sPath ) then
+            if not os.loadAPI( sPath ) then
+                bAPIError = true
+            end
+        end
+    end
+end
+
+if pocket and fs.isDir( "/lib/apis/pocket" ) then
+    -- Load pocket APIs
+    local tApis = fs.list( "/lib/apis/pocket" )
+    for n,sFile in ipairs( tApis ) do
+        if string.sub( sFile, 1, 1 ) ~= "." then
+            local sPath = fs.combine( "/lib/apis/pocket", sFile )
+            if not fs.isDir( sPath ) then
+                if not os.loadAPI( sPath ) then
+                    bAPIError = true
+                end
+            end
+        end
+    end
+end
+
+if bAPIError then
+    print( "Press any key to continue" )
+    os.pullEvent( "key" )
+    term.clear()
+    term.setCursorPos( 1,1 )
+end
+
+-- Load user settings
+if fs.exists( ".settings" ) then
+    settings.load( ".settings" )
+end
+
+-- Run the OS
+local ok, err = pcall( function()
+    parallel.waitForAny( 
+        function()
+            if term.isColour() then
+                os.run({}, "/sbin/shell/shell.lua")
+            else
+                printError("Use advanced computer...")
+                sleep(3)
+                term.setTextColor(colors.green)
+                print("SL.shutdownService")
+                os.run({},"/sys/serv/shutdown.lua")
+            end
+            print( "Internal error" )
+            print( "Press any key to continue" )
+            os.pullEvent( "key" )
+            term.setTextColor(colors.green)
+            print("SL.shutdownService")
+            os.run( {}, "/sys/serv/shutdown.lua" )
+        end,
+        function()
+            while true do
+                sleep(1)
+            end
+        end 
+    )
+    
+end )
+
+-- If the OS errored, let the user read it.
+term.redirect( term.native() )
+if not ok then
+    printError( err )
+    pcall( function()
+        term.setCursorBlink( false )
+        print( "Press any key to continue" )
+        os.pullEvent( "key" )
+    end )
+end
+
+-- End
+os.shutdown()
