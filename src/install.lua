f4e034a68f31c6d0276595b9e6e9fb90403dfba4
Astronand <hunter.vinson@fwcsstudents.org>
2025-02-21 10:55:43 -0500

    e

---
+++ src/install.lua
@@ -0,0 +1,44 @@
+--[[
+Made for use in StarlightOS
+filed under GNU General Public License.
+    Copyright (C) 2025  StarlightOS
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+    contacts-
+      <https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/legal/contacts.md>
+]]
+local API = "https://api.github.com/repos/ASTRONAND/Starlight-OS/contents/src/root/"
+os.pullEvent = os.pullEventRaw
+term.setPaletteColor(colors.red,0xff0000)
+term.setPaletteColor(colors.green,0x00ff00)
+term.setPaletteColor(colors.blue,0x0000ff)
+term.clear()
+term.setCursorPos(1,1)
+term.write("Welcome to")
+term.setTextColor(colors.blue)
+term.write(" Starlight OS")
+print("")
+print("")
+print("Updating Installer")
+local file = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/src/install.lua")
+local fh = fs.open("sbin/SLInstall.lua", "w")
+fh.write(file.readAll())
+fh.close()
+sleep(1)
+term.setTextColor(colors.green)
+print("SL.install service started")
+term.setTextColor(colors.cyan)
+print("Installing")
+shell.run("sbin/SLInstall.lua")
