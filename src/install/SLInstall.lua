f20490946069a7f1b6dcc7b8c733dfbfd645c6cb
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-02-23 05:00:05 -0500

    Update SLInstall.lua

--- src/install/SLInstall.lua
+++ src/install/SLInstall.lua
@@ -151,7 +151,7 @@ sleep(1.5)
 term.setBackgroundColor(colors.blue)
 term.clear()
 term.setCursorPos(1,1)
-textutils.pagedPrintSkip(Copyright.readAll(),keys.s)
+textutils.pagedPrintSkip(Copyright.readAll(),"s")
 print("")
 print("(Y/N)")
 while true do
