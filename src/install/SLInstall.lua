00414de2a8c1912458cb30ce4f6eb7ea37ecf0ae
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-02-23 11:14:16 -0500

    e

--- src/install/SLInstall.lua
+++ src/install/SLInstall.lua
@@ -29,6 +29,188 @@ local VER = "src"
 local Copyright = http.get("https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/"..VER.."/install/TOSPrint.txt")
 local API = "https://api.github.com/repos/ASTRONAND/Starlight-OS/contents/"
 local json = load(http.get("https://raw.githubusercontent.com/Starlight-CC/Starlight-OS/refs/heads/main/src/root/lib/sys/json.la").readAll())()
+-- PrimeUI by JackMacWindows
+-- Public domain/CC0
+
+local expect = require "cc.expect".expect
+
+-- Initialization code
+local PrimeUI = {}
+do
+    local coros = {}
+    local restoreCursor
+
+    --- Adds a task to run in the main loop.
+    ---@param func function The function to run, usually an `os.pullEvent` loop
+    function PrimeUI.addTask(func)
+        expect(1, func, "function")
+        local t = {coro = coroutine.create(func)}
+        coros[#coros+1] = t
+        _, t.filter = coroutine.resume(t.coro)
+    end
+
+    --- Sends the provided arguments to the run loop, where they will be returned.
+    ---@param ... any The parameters to send
+    function PrimeUI.resolve(...)
+        coroutine.yield(coros, ...)
+    end
+
+    --- Clears the screen and resets all components. Do not use any previously
+    --- created components after calling this function.
+    function PrimeUI.clear()
+        -- Reset the screen.
+        term.setCursorPos(1, 1)
+        term.setCursorBlink(false)
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+        term.clear()
+        -- Reset the task list and cursor restore function.
+        coros = {}
+        restoreCursor = nil
+    end
+
+    --- Sets or clears the window that holds where the cursor should be.
+    ---@param win window|nil The window to set as the active window
+    function PrimeUI.setCursorWindow(win)
+        expect(1, win, "table", "nil")
+        restoreCursor = win and win.restoreCursor
+    end
+
+    --- Gets the absolute position of a coordinate relative to a window.
+    ---@param win window The window to check
+    ---@param x number The relative X position of the point
+    ---@param y number The relative Y position of the point
+    ---@return number x The absolute X position of the window
+    ---@return number y The absolute Y position of the window
+    function PrimeUI.getWindowPos(win, x, y)
+        if win == term then return x, y end
+        while win ~= term.native() and win ~= term.current() do
+            if not win.getPosition then return x, y end
+            local wx, wy = win.getPosition()
+            x, y = x + wx - 1, y + wy - 1
+            _, win = debug.getupvalue(select(2, debug.getupvalue(win.isColor, 1)), 1) -- gets the parent window through an upvalue
+        end
+        return x, y
+    end
+
+    --- Runs the main loop, returning information on an action.
+    ---@return any ... The result of the coroutine that exited
+    function PrimeUI.run()
+        while true do
+            -- Restore the cursor and wait for the next event.
+            if restoreCursor then restoreCursor() end
+            local ev = table.pack(os.pullEvent())
+            -- Run all coroutines.
+            for _, v in ipairs(coros) do
+                if v.filter == nil or v.filter == ev[1] then
+                    -- Resume the coroutine, passing the current event.
+                    local res = table.pack(coroutine.resume(v.coro, table.unpack(ev, 1, ev.n)))
+                    -- If the call failed, bail out. Coroutines should never exit.
+                    if not res[1] then error(res[2], 2) end
+                    -- If the coroutine resolved, return its values.
+                    if res[2] == coros then return table.unpack(res, 3, res.n) end
+                    -- Set the next event filter.
+                    v.filter = res[2]
+                end
+            end
+        end
+    end
+end
+
+--- Creates a scrollable window, which allows drawing large content in a small area.
+---@param win window The parent window of the scroll box
+---@param x number The X position of the box
+---@param y number The Y position of the box
+---@param width number The width of the box
+---@param height number The height of the outer box
+---@param innerHeight number The height of the inner scroll area
+---@param allowArrowKeys boolean|nil Whether to allow arrow keys to scroll the box (defaults to true)
+---@param showScrollIndicators boolean|nil Whether to show arrow indicators on the right side when scrolling is available, which reduces the inner width by 1 (defaults to false)
+---@param fgColor number|nil The color of scroll indicators (defaults to white)
+---@param bgColor color|nil The color of the background (defaults to black)
+---@return window inner The inner window to draw inside
+---@return fun(pos:number) scroll A function to manually set the scroll position of the window
+function PrimeUI.scrollBox(win, x, y, width, height, innerHeight, allowArrowKeys, showScrollIndicators, fgColor, bgColor)
+    expect(1, win, "table")
+    expect(2, x, "number")
+    expect(3, y, "number")
+    expect(4, width, "number")
+    expect(5, height, "number")
+    expect(6, innerHeight, "number")
+    expect(7, allowArrowKeys, "boolean", "nil")
+    expect(8, showScrollIndicators, "boolean", "nil")
+    fgColor = expect(9, fgColor, "number", "nil") or colors.white
+    bgColor = expect(10, bgColor, "number", "nil") or colors.black
+    if allowArrowKeys == nil then allowArrowKeys = true end
+    -- Create the outer container box.
+    local outer = window.create(win == term and term.current() or win, x, y, width, height)
+    outer.setBackgroundColor(bgColor)
+    outer.clear()
+    -- Create the inner scrolling box.
+    local inner = window.create(outer, 1, 1, width - (showScrollIndicators and 1 or 0), innerHeight)
+    inner.setBackgroundColor(bgColor)
+    inner.clear()
+    -- Draw scroll indicators if desired.
+    if showScrollIndicators then
+        outer.setBackgroundColor(bgColor)
+        outer.setTextColor(fgColor)
+        outer.setCursorPos(width, height)
+        outer.write(innerHeight > height and "\31" or " ")
+    end
+    -- Get the absolute position of the window.
+    x, y = PrimeUI.getWindowPos(win, x, y)
+    -- Add the scroll handler.
+    local scrollPos = 1
+    PrimeUI.addTask(function()
+        while true do
+            -- Wait for next event.
+            local ev = table.pack(os.pullEvent())
+            -- Update inner height in case it changed.
+            innerHeight = select(2, inner.getSize())
+            -- Check for scroll events and set direction.
+            local dir
+            if ev[1] == "key" and allowArrowKeys then
+                if ev[2] == keys.up then dir = -1
+                elseif ev[2] == keys.down then dir = 1 end
+            elseif ev[1] == "mouse_scroll" and ev[3] >= x and ev[3] < x + width and ev[4] >= y and ev[4] < y + height then
+                dir = ev[2]
+            end
+            -- If there's a scroll event, move the window vertically.
+            if dir and (scrollPos + dir >= 1 and scrollPos + dir <= innerHeight - height) then
+                scrollPos = scrollPos + dir
+                inner.reposition(1, 2 - scrollPos)
+            end
+            -- Redraw scroll indicators if desired.
+            if showScrollIndicators then
+                outer.setBackgroundColor(bgColor)
+                outer.setTextColor(fgColor)
+                outer.setCursorPos(width, 1)
+                outer.write(scrollPos > 1 and "\30" or " ")
+                outer.setCursorPos(width, height)
+                outer.write(scrollPos < innerHeight - height and "\31" or " ")
+            end
+        end
+    end)
+    -- Make a function to allow external scrolling.
+    local function scroll(pos)
+        expect(1, pos, "number")
+        pos = math.floor(pos)
+        expect.range(pos, 1, innerHeight - height)
+        -- Scroll the window.
+        scrollPos = pos
+        inner.reposition(1, 2 - scrollPos)
+        -- Redraw scroll indicators if desired.
+        if showScrollIndicators then
+            outer.setBackgroundColor(bgColor)
+            outer.setTextColor(fgColor)
+            outer.setCursorPos(width, 1)
+            outer.write(scrollPos > 1 and "\30" or " ")
+            outer.setCursorPos(width, height)
+            outer.write(scrollPos < innerHeight - height and "\31" or " ")
+        end
+    end
+    return inner, scroll
+end
 
 local pgk_env = setmetatable({}, { __index = _ENV })
 pgk_env.require = dofile("rom/modules/main/cc/require.lua").make(pgk_env, "rom/modules/main")
@@ -85,162 +267,13 @@ local function deleteFiles(directory, exceptions)
     ["sbin"] = true
   }
 
-function write(sText)
-    expect(1, sText, "string", "number")
-
-    local w, h = term.getSize()
-    local x, y = term.getCursorPos()
-
-    local nLinesPrinted = 0
-    local function newLine()
-        if y + 1 <= h then
-            term.setCursorPos(1, y + 1)
-        else
-            term.setCursorPos(1, h)
-            term.scroll(1)
-        end
-        x, y = term.getCursorPos()
-        nLinesPrinted = nLinesPrinted + 1
-    end
-
-    -- Print the line with proper word wrapping
-    sText = tostring(sText)
-    while #sText > 0 do
-        local whitespace = string.match(sText, "^[ \t]+")
-        if whitespace then
-            -- Print whitespace
-            term.write(whitespace)
-            x, y = term.getCursorPos()
-            sText = string.sub(sText, #whitespace + 1)
-        end
-
-        local newline = string.match(sText, "^\n")
-        if newline then
-            -- Print newlines
-            newLine()
-            sText = string.sub(sText, 2)
-        end
-
-        local text = string.match(sText, "^[^ \t\n]+")
-        if text then
-            sText = string.sub(sText, #text + 1)
-            if #text > w then
-                -- Print a multiline word
-                while #text > 0 do
-                    if x > w then
-                        newLine()
-                    end
-		    s = term.write(text)
-                    if s == true then
-			break
-		    end
-                    text = string.sub(text, w - x + 2)
-                    x, y = term.getCursorPos()
-                end
-            else
-                -- Print a word normally
-                if x + #text - 1 > w then
-                    newLine()
-                end
-                s = term.write(text)
-		if s == true then
-			break
-		    end
-                x, y = term.getCursorPos()
-            end
-        end
-    end
-
-    return nLinesPrinted
-end
-
-function print(...)
-    local nLinesPrinted = 0
-    local nLimit = select("#", ...)
-    for n = 1, nLimit do
-        local s = tostring(select(n, ...))
-        if n < nLimit then
-            s = s .. "\t"
-        end
-        nLinesPrinted = nLinesPrinted + write(s)
-    end
-    nLinesPrinted = nLinesPrinted + write("\n")
-    return nLinesPrinted
-end
-
-local function makePagedScrollS(_term, _nFreeLines)
-    local nativeScroll = _term.scroll
-    local nFreeLines = _nFreeLines or 0
-    return function(_n)
-        for _ = 1, _n do
-            nativeScroll(1)
-	    skip = false
-            if nFreeLines <= 0 then
-                local _, h = _term.getSize()
-                _term.setCursorPos(1, h)
-                _term.write("Press any key to continue, press s to skip, press q to quit")
-                _,k = os.pullEvent("key") 
-                if k == keys.s then
-		    skip = true
-                    break
-                elseif k == keys.q then
-		    term.setBackgroundColor(colors.black)
-		    term.clear()
-		    term.setCursorPos(1,1)
-                    error("Install terminated",0)
-		else
-                end 
-                _term.clearLine()
-                _term.setCursorPos(1, h)
-            else
-                nFreeLines = nFreeLines - 1
-            end
-        end
-    	return skip
-    end
-end
-
-local function pagedPrintS(text, free_lines)
-    expect(2, free_lines, "number", "nil")
-    -- Setup a redirector
-    local oldTerm = term.current()
-    local newTerm = {}
-    for k, v in pairs(oldTerm) do
-        newTerm[k] = v
-    end
-    newTerm.scroll = makePagedScrollS(oldTerm, free_lines)
-    term.redirect(newTerm)
-
-    -- Print the text
-    local result
-    local ok, err = pcall(function()
-        if text ~= nil then
-            result = print(text)
-        else
-            result = print()
-        end
-    end)
-
-    -- Removed the redirector
-    term.redirect(oldTerm)
-
-    -- Propogate errors
-    if not ok then
-        error(err, 0)
-    end
-    return result
-end
-
-function textutils.pagedPrintSkip(s)
-    pagedPrintS(s)
-end
 term.setTextColor(colors.white)
 print("Connecting to "..API)
 sleep(1.5)
 term.setBackgroundColor(colors.blue)
-term.clear()
-term.setCursorPos(1,1)
-textutils.pagedPrintSkip(Copyright.readAll())
+PrimeUI.clear()
+local scroller = PrimeUI.scrollBox(term.current(), 4, 6, 40, 10, 9000, true, true, colors.white, colors.blue)
+PrimeUI.drawText(scroller, Copyright.readAll(), true)
 print("")
 print("(Y/N)")
 while true do
