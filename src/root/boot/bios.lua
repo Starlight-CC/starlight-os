0c3ff90eef8889ae52fa0f30077391a7899f32c9
Astronand <hunter.vinson@fwcsstudents.org>
2025-03-13 08:27:36 -0400

    e

--- src/root/boot/bios.lua
+++ src/root/boot/bios.lua
@@ -3,19 +3,19 @@ local ok, err = pcall( function()
     parallel.waitForAny( 
         function()
             if term.isColour() then
-                os.run({}, "/boot/kernel.lua")
+                os.run({}, "/boot/SLKernel.sys")
             else
                 printError("Sorry this OS is only for advanced computers")
                 sleep(3)
                 term.setTextColor(colors.green)
                 print("SL.shutdownService")
-                os.run({},"/sys/serv/shutdown.lua")
+                os.run({},"/sys/serv/shutdown.sys")
             end
             print( "Press any key to continue" )
             os.pullEvent( "key" )
             term.setTextColor(colors.green)
             print("SL.rebootService")
-            os.run( {}, "/sys/serv/reboot.lua" )
+            os.run( {}, "/sys/serv/reboot.sys" )
         end,
         function()
             while true do
