3614d871974b4f73c78bf9ac64530e95247b6a7e
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-21 09:20:06 -0400

    e

--- src/root/startup.lua
+++ src/root/startup.lua
@@ -1,3 +1,56 @@
 --Copyright (C) 2025  Starlight-CC
 sleep(0)
-shell.run("/boot/PXBoot.sys /ect/PXBoot/bootconfig.conf")
+local w,h = term.getSize()
+local function printCentered(h,string)
+    term.setCursorPos((w/2)-(#string/2),h)
+    term.write(string)
+end
+printCentered(h-2,"Press alt+1 to enter boot menu")
+term.setTextColor(colors.cyan)
+os.startTimer(1.2)
+printCentered(h/2,"S")
+sleep(.1)
+printCentered(h/2,"St")
+sleep(.1)
+printCentered(h/2,"Sta")
+sleep(.1)
+printCentered(h/2,"Star")
+sleep(.1)
+printCentered(h/2,"Starl")
+sleep(.1)
+printCentered(h/2,"Starli")
+sleep(.1)
+printCentered(h/2,"Starlig")
+sleep(.1)
+printCentered(h/2,"Starligh")
+sleep(.1)
+printCentered(h/2,"Starlight")
+sleep(.2)
+local heldCtrl, heldAlt, heldShift, result = false, false, false, false
+while true do
+    local event, param1, param2 = os.pullEvent() -- wait for key
+    if event == "key" then
+        -- check if key is down, all modifiers are correct, and that it's not held
+        if param1 == key and heldCtrl == withCtrl and heldAlt == withAlt and heldShift == withShift and not param2 then
+            result = false
+            break
+        end
+        -- activate modifier keys
+        elseif param1 == keys.leftCtrl or param1 == keys.rightCtrl then heldCtrl = true
+        elseif param1 == keys.leftAlt or param1 == keys.rightAlt then heldAlt = true
+        elseif param1 == keys.leftShift or param1 == keys.rightShift then heldShift = true end
+    elseif event == "key_up" then
+        -- deactivate modifier keys
+        if param1 == keys.leftCtrl or param1 == keys.rightCtrl then heldCtrl = false
+        elseif param1 == keys.leftAlt or param1 == keys.rightAlt then heldAlt = false
+        elseif param1 == keys.leftShift or param1 == keys.rightShift then heldShift = false end
+    elseif event == "timer" then
+        result = true
+        break
+    end
+end
+if result == true then
+    shell.run("/boot/PXBoot.sys /ect/PXBoot/bootconfig.conf -i")
+else
+    shell.run("/boot/PXBoot.sys /ect/PXBoot/bootconfig.conf")
+end
