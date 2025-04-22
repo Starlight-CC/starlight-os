8f5d08c561751dfc9316a3daac0a96a89328e522
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-22 10:40:13 -0400

    e

--- src/root/sbin/registeryEditor/init.lua
+++ src/root/sbin/registeryEditor/init.lua
@@ -2,5 +2,5 @@ local tArgs = {...}
 local PrimeUI = dofile("/lib/PrimeUI.la")
 PrimeUI.clear()
 local w,h = term.getSize()
-PrimeUI.borderBox(term.current(),2,h-2,w-2,2, colors.white, colors.blue)
+
 PrimeUI.run()
