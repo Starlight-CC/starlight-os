a8d17d17ed16194206db51c40267a9b2085cfa3e
Astronand <hunter.vinson@fwcsstudents.org>
2025-05-08 09:02:12 -0400

    e

--- tools/SLCompiler.lua
+++ tools/SLCompiler.lua
@@ -1,24 +1,4 @@
---[[
-Made for use in StarlightOS
-filed under GNU General Public License.
-    Copyright (C) 2025  StarlightOS
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>
-
-    contacts-
-      <https://raw.githubusercontent.com/ASTRONAND/Starlight-OS/refs/heads/main/legal/contacts.md>
-]]
+--Copyright (C) 2025  Starlight-CC
 term.setPaletteColor(colors.red,0xff0000)
 term.setPaletteColor(colors.green,0x00ff00)
 term.setPaletteColor(colors.blue,0x000000)
@@ -213,6 +193,7 @@ installFs(FS)
 term.setTextColor(colors.gray)
 sleep(.2)
 shell.run("tmp/setup.lua")
+fs.delete("tmp/setup.lua")
 print("Rebooting ...")
 sleep(1)
 term.setTextColor(colors.green)
